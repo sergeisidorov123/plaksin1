5b1578f185dfcd1f8e4beab1fd1144b3ff6645d6
sergeisidorov123 <seregasidorov2015@mail.ru>
2024-09-17 00:20:34 +0500

    plaksin1

--- main.py
+++ main.py
@@ -1,21 +1,126 @@
 import cmath
 import sympy
 import telebot
-
+import os
 
 TOKEN = '7240415310:AAGOoQe_ln15vU2icX_w574-4uEm2XuxTXI'
 
 bot = telebot.TeleBot(TOKEN)
 
+user_language = {}
+
+
+def load_language(language_code):
+    lang_file_path = f'C:/Users/111/PycharmProjects/proektniiseminar1/lang/{language_code}.txt'
+    print(f"Загрузка файла языка: {lang_file_path}")
+    if os.path.exists(lang_file_path):
+        with open(lang_file_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+        lang_dict = {}
+        for line in lines:
+            key, value = line.strip().split('=')
+            lang_dict[key] = value
+        print(f"Загруженные строки: {lang_dict}")
+        return lang_dict
+    else:
+        print(f"Файл языка не найден: {lang_file_path}")
+        return None
+
+
+def get_translation(user_id, key):
+    language_code = user_language.get(user_id, 'ru')
+    translations = load_language(language_code)
+    if translations and key in translations:
+        return translations[key]
+    return None
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row('Русский', 'English', 'Español')
+    bot.send_message(message.chat.id, "Выберите язык:", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text in ['Русский', 'English', 'Español'])
+def handle_language_choice(message):
+    if message.text == 'Русский':
+        user_language[message.chat.id] = 'ru'
+    elif message.text == 'English':
+        user_language[message.chat.id] = 'en'
+    elif message.text == 'Español':
+        user_language[message.chat.id] = 'es'
+
+    welcome_text = get_translation(message.chat.id, 'welcome')
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row(get_translation(message.chat.id, 'arithmetic'),
+               get_translation(message.chat.id, 'complex'),
+               get_translation(message.chat.id, 'analytic'))
+    bot.send_message(message.chat.id, welcome_text, reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text == get_translation(message.chat.id, 'arithmetic'))
+def handle_arithmetic_command(message):
+    bot.send_message(message.chat.id, get_translation(message.chat.id, "arithmetic_instructions"))
+    bot.register_next_step_handler(message, process_arithmetic)
+
+
+@bot.message_handler(func=lambda message: message.text == get_translation(message.chat.id, 'complex'))
+def handle_complex_command(message):
+    bot.send_message(message.chat.id, get_translation(message.chat.id, "complex_instructions"))
+    bot.register_next_step_handler(message, process_complex)
+
+
+@bot.message_handler(func=lambda message: message.text == get_translation(message.chat.id, 'analytic'))
+def handle_analytic_command(message):
+    # Отправляем сообщение о вводе выражения для извлечения корня
+    bot.send_message(message.chat.id, get_translation(message.chat.id, "enter_expression"))
+    bot.register_next_step_handler(message, process_analytical)
+
+
+def process_arithmetic(message):
+    try:
+        num, accuracy = map(float, message.text.split(","))
+        if accuracy < 0:
+            bot.send_message(message.chat.id, get_translation(message.chat.id, "error_accuracy"))
+            return
+        sqrt_result = sqrt_with_accuracy(num, int(accuracy))
+        bot.send_message(message.chat.id, f"+{sqrt_result}, -{sqrt_result}")
+    except ValueError:
+        bot.send_message(message.chat.id, get_translation(message.chat.id, "error_input"))
+    except Exception as e:
+        bot.send_message(message.chat.id, get_translation(message.chat.id, "error_general") + str(e))
+
+
+def process_complex(message):
+    try:
+        real_part, imaginary_part, decimal_places = map(float, message.text.split(","))
+        complex_number = complex(real_part, imaginary_part)
+        result = sqrt_of_complex(complex_number, int(decimal_places))
+        bot.send_message(message.chat.id, f"{result}")
+    except Exception as e:
+        bot.send_message(message.chat.id, get_translation(message.chat.id, "error_input") + str(e))
+
+
+def process_analytical(message):
+    try:
+        result = sqrt_of_analitics(message.text)
+        bot.send_message(message.chat.id, f"{result}")
+    except Exception as e:
+        bot.send_message(message.chat.id, get_translation(message.chat.id, "error_analytical") + str(e))
+
 
 def sqrt_with_accuracy(num, accuracy):
-    precision = 10**(-accuracy)
+    if accuracy < 0:
+        raise ValueError("Точность не может быть отрицательной")
+
+    precision = 10 ** (-accuracy)
     low = 0
     high = num
     mid = (low + high) / 2
 
-    while abs(mid**2 - num) > precision:
-        if mid**2 < num:
+    while abs(mid ** 2 - num) > precision:
+        if mid ** 2 < num:
             low = mid
         else:
             high = mid
@@ -28,62 +133,17 @@ def sqrt_of_complex(number, decimal_places):
     sqrt_result = cmath.sqrt(number)
     real_part = round(sqrt_result.real, decimal_places)
     imaginary_part = round(sqrt_result.imag, decimal_places)
+
     return complex(real_part, imaginary_part)
 
 
 def sqrt_of_analitics(expression_str):
-    x = sympy.symbols('x')
     expression = sympy.sympify(expression_str)
     sqrt_result = sympy.sqrt(expression)
+
     simplified_result = sympy.simplify(sqrt_result)
+
     return simplified_result
-
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.row('Арифметический корень', 'Комплексный корень', 'Аналитический корень')
-    bot.send_message(message.chat.id, ".", reply_markup=markup)
-
-
-@bot.message_handler(func=lambda message: message.text in ['Арифметический корень', 'Комплексный корень', 'Аналитический корень'])
-def handle_choice(message):
-    if message.text == 'Арифметический корень':
-        bot.send_message(message.chat.id, "Введите число и точность (через запятую, например 16, 4):")
-        bot.register_next_step_handler(message, handle_arithmetic)
-    elif message.text == 'Комплексный корень':
-        bot.send_message(message.chat.id, "Введите действительную и мнимую части числа и количество знаков (например: 3, 4, 2):")
-        bot.register_next_step_handler(message, handle_complex)
-    elif message.text == 'Аналитический корень':
-        bot.send_message(message.chat.id, "Введите аналитическое выражение (например: x**2 + 4*x + 4):")
-        bot.register_next_step_handler(message, handle_analytical)
-
-
-def handle_arithmetic(message):
-    try:
-        num, accuracy = map(float, message.text.split(","))
-        result = sqrt_with_accuracy(num, int(accuracy))
-        bot.send_message(message.chat.id, f"Арифметический квадратный корень: {result}")
-    except:
-        bot.send_message(message.chat.id, "Ошибка ввода. Пожалуйста, введите два числа через запятую.")
-
-
-def handle_complex(message):
-    try:
-        real_part, imaginary_part, decimal_places = map(float, message.text.split(","))
-        complex_number = complex(real_part, imaginary_part)
-        result = sqrt_of_complex(complex_number, int(decimal_places))
-        bot.send_message(message.chat.id, f"Комплексный квадратный корень: {result}")
-    except:
-        bot.send_message(message.chat.id, "Ошибка ввода. Пожалуйста, введите три числа через запятую.")
-
-
-def handle_analytical(message):
-    try:
-        result = sqrt_of_analitics(message.text)
-        bot.send_message(message.chat.id, f"Аналитический квадратный корень: {result}")
-    except:
-        bot.send_message(message.chat.id, "Ошибка при обработке аналитического выражения.")
 
 
 if __name__ == '__main__':
